7092daa89a7583a29603cadac94ded8bc5bbc5ec
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2020-12-25 12:45:19 -0800

    Adding more extension methods soon....

---
+++ WinForms/Extensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows.Forms;
+
+namespace System.NET.CSharp.WinForms
+{
+	public static class Extensions
+	{
+		#region Readable Exceptions
+		public static DialogResult ToMessage(this Exception exception, string Title)
+		{
+			return MessageBox.Show($"Error Detected:\n\n{exception.Message}", Title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+		public static DialogResult ToMessage(this Exception exception, MessageBoxButtons buttons)
+		{
+			return MessageBox.Show($"Error Detected:\n\n{exception.Message}", "Error Found", buttons, MessageBoxIcon.Error);
+		}
+		public static DialogResult ToMessage(this Exception exception, MessageBoxIcon icon)
+		{
+			return MessageBox.Show($"Error Detected:\n\n{exception.Message}", "Error Found", MessageBoxButtons.OK, icon);
+		}
+		public static DialogResult ToMessage(this Exception exception, string Title, MessageBoxButtons buttons)
+		{
+			return MessageBox.Show($"Error Detected:\n\n{exception.Message}", Title, buttons, MessageBoxIcon.Error);
+		}
+		public static DialogResult ToMessage(this Exception exception, string Title, MessageBoxIcon icon)
+		{
+			return MessageBox.Show($"Error Detected:\n\n{exception.Message}", Title, MessageBoxButtons.OK, icon);
+		}
+		public static DialogResult ToMessage(this Exception exception, string Title, MessageBoxButtons buttons, MessageBoxIcon icon)
+		{
+			return MessageBox.Show($"Error Detected:\n\n{exception.Message}", Title, buttons, icon);
+		}
+		#endregion
+		#region Video Player
+		public static int PlayYouTubeVideo(this WebBrowser browser, string videoID)
+		{
+			browser.DocumentText += $"<html>\n" +
+				$"\t<body>\n" +
+				$"\t\t<iframe id=video src=\"https://www.youtube.com/embed/{videoID}\" width={browser.Width - 21} height={browser.Height - 21} frameborder=0>\n" +
+				$"\t\t</iframe>\n" +
+				$"\t</body>\n" +
+				$"</html>";
+			return 0;
+		}
+		#endregion
+	}
+}
