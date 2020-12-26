ed1ddf18a37e5a2df84bc6b1e848229333a784db
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2020-12-25 20:01:51 -0800

    You can now record yourself talking

---
+++ Windows/Audio.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.InteropServices;
+
+namespace System.NET.CSharp.Windows
+{
+	public class Audio
+	{
+		private AudioFile audioFile;
+
+		public AudioFile AudioFile => audioFile;
+
+		[DllImport("winmm", EntryPoint = "mciSendStringA")]
+		private static extern int record(string lpstrCommand, string lpstrReturnString, int uReturnLength, int handleCallBack);
+
+		private void record(string lpstrCommand)
+		{
+			record(lpstrCommand, "", 0, 0);
+		}
+
+		public Audio(string resultFile)
+		{
+			audioFile = new AudioFile(resultFile);
+		}
+		~Audio()
+		{
+			Delete();
+		}
+		
+		public void RecordMicrophone()
+		{
+			record("open new Type waveaudio Alias recsound");
+			record("record recsound");
+		}
+		public void StopRecording()
+		{
+			record($"save recsound {audioFile.Path}");
+			record("close recsound");
+		}
+		public void CancelRecording()
+		{
+			try
+			{
+				StopRecording();
+				audioFile.Delete();
+			} catch {}
+		}
+		public void Delete()
+		{
+			CancelRecording();
+			audioFile = new AudioFile();
+		}
+	}
+}
---
+++ Windows/AudioFile.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace System.NET.CSharp.Windows
+{
+	public struct AudioFile
+	{
+		private string path;
+
+		public string Path => path;
+
+		public AudioFile(string path)
+		{
+			this.path = path;
+		}
+
+		public void Play()
+		{
+			new Microsoft.VisualBasic.Devices.Audio().Play(path);
+		}
+		public void Delete()
+		{
+			File.Delete(path);
+		}
+	}
+}
