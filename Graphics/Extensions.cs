7f58ff44cb48fde2fdea2534c9ed4547d5aaadf2
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2021-01-03 19:41:04 -0800

    added new color conversion system

--- Graphics/Extensions.cs
+++ Graphics/Extensions.cs
@@ -36,5 +36,38 @@ namespace System.NET.CSharp.Graphics
 				color.B + rgbAmount <= 255 ? color.B + rgbAmount : color.B
 				);
 		}
+
+		public static int ToWin32(this Color c)
+		{
+			return ColorTranslator.ToWin32(c);
+		}
+		public static string ToHtml(this Color c)
+		{
+			return ColorTranslator.ToHtml(c);
+		}
+		
+		public static Color ToRGB(this int win32)
+		{
+			return ColorTranslator.FromWin32(win32);
+		}
+		public static string ToHtml(this int win32)
+		{
+			return ColorTranslator.ToHtml(ColorTranslator.FromWin32(win32));
+		}
+		
+		public static Color ToRGB(this string html)
+		{
+			return ColorTranslator.FromHtml(html);
+		}
+		public static int ToWin32(this string html)
+		{
+			return ColorTranslator.ToWin32(ColorTranslator.FromHtml(html));
+		}
+
+		public static Color ToColor(this int hexCode)
+		{
+			string hex = hexCode.ToString().Substring(2);
+			return hex.ToRGB();
+		}
 	}
 }
