edeffc35a67f4f98975971e9c2a40fab008b81c6
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2021-01-04 17:33:32 -0800

    added class to manipulate files

---
+++ FileSystem/File.cs
@@ -0,0 +1,37 @@
+﻿using System.NET.CSharp.General;
+
+namespace System.NET.CSharp.FileSystem
+{
+	public class File
+	{
+		private string filePath;
+
+		public File(string filePath)
+		{
+			this.filePath = filePath;
+		}
+
+		public void Write(string line)
+		{
+			IO.File.WriteAllText(filePath, line);
+		}
+		public void WriteLine(string line)
+		{
+			IO.File.WriteAllText(filePath, line + "\n");
+		}
+
+		public string ReadLines()
+		{
+			return IO.File.ReadAllText(filePath);
+		}
+		public int ReadNumbers()
+		{
+			return IO.File.ReadAllText(filePath).ToInt();
+		}
+
+		public void Delete()
+		{
+			IO.File.Delete(filePath);
+		}
+	}
+}
